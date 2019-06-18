b54fd26e0e4dd4c19b45ac3be24c6add2fe58575
Sergio Heynes <sergio.heynes@gmail.com>
2019-06-18 08:37:58 -0500

    Functionality to save data added.

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,22 @@
-const products = [];
+const Product = require('../model/product');
 
 exports.getAddProducts = (req, res, next) => {
     res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 };
 
 exports.postAddProducts = (req, res, next) => {
-    products.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/'
+        });
+    });
+    
 };
