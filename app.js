ea398752637270b924598fa9205a0137a83c8f66
Sergio Heynes <sergio.heynes@gmail.com>
2019-07-05 15:46:32 -0500

    Added new products to the cart. Added existing products and retrieved cart items. Deleted related items and deteled cart products.

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ Cart.belongsToMany(Product, { through: CartItem } );
 Product.belongsToMany(Cart, { through: CartItem } );
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result)
@@ -64,11 +64,14 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000, () => {
       console.log('');
       console.log('***************************');
       console.log('Server started on Port 3000');
-      });
+    });
   })
   .catch(err => 
     console.log(err)
